7557fb3b551d22ade84ed91ebae6716397eeac7a
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-01-25 22:19:48 +0300

    Add files via upload

---
+++ Tema15prkt1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema14prkt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sport[] pupils = new Sport[3];
+            //for (int i = 1; i < pupils.Length; i++)
+            //{
+            //    Sport pupils1 = new Sport();
+            //    Console.Write($"Введите Ф.И.О {i} ученика - ");
+            //    pupils1.Fio = Console.ReadLine();
+            //    Console.Write("Введите пол ученика - ");
+            //    pupils1.Pol = Console.ReadLine();
+            //    Console.Write("Введите дату рождения ученика - ");
+            //    pupils1.Data = int.Parse(Console.ReadLine());
+            //    Console.Write(pupils1.ToString());
+            //    if (pupils1.Data > 2018)
+            //    {
+            //        Console.Write("Вы ошиблись датой\n");
+            //    }
+            //}
+            //Не понял как подсчитать кол-во девочек и мальчиков
+            //Задание 2
+            Sport[] sportsmans = new Sport[3];
+            int c = 0;
+            for( int y = 0;y < sportsmans.Length;y++ )
+            {
+                Sport sportsmans1 = new Sport("Artem",2,3);
+                Console.Write($"Введите Ф.И.О {y} ученика - ");
+                sportsmans1.Fio2 = Console.ReadLine();
+                Console.Write("Введите рост спортсмена - ");
+                sportsmans1.Height = int.Parse(Console.ReadLine());
+                Console.Write("Введите вес спортсмена - ");
+                sportsmans1.Weight = double.Parse(Console.ReadLine());
+                if(sportsmans1.Weight > 70)
+                {
+                    c++;
+                }
+                Console.WriteLine($"Кол-во спортсменов у которых вес 70+ = {c}");
+                
+                sportsmans1.Print();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema15prkt1/Sport.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema14prkt2
+{
+    class Sport
+    {
+        /// <summary>
+        /// статические поля
+        /// </summary>
+        //static string fio;
+        //static string pol;
+        //static int data;
+        /// <summary>
+        /// Свойства
+        /// </summary>
+        //public string Fio
+        //{
+        //    get { return fio; }
+        //    set { fio = value; }
+        //}
+        //public string Pol
+        //{
+        //    get { return pol; }
+        //    set { pol = value; }
+        //}
+        //public int Data
+        //{
+        //    get { return data; }
+        //    set { data = value; }
+        //}
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        //public Sport()
+        //{
+        //    this.Fio = fio;
+        //    this.Pol = pol;
+        //    this.Data = data;
+        //}
+
+        /// <summary>
+        /// Вывод информации
+        /// </summary>
+        /// <returns></returns>
+        //public override string ToString()
+        //{
+        //    return $"Ф.И.О - {Fio}\nПол -{Pol}\nДата рождения - {Data}\n";
+        //}
+
+        //Задание 2
+        /// <summary>
+        /// Статические поля 
+        /// </summary>
+        static string fio2;
+        static int height;
+        static double weight;
+        
+        /// <summary>
+        /// Свойства для статических полей
+        /// </summary>
+        public string Fio2
+        {
+            get { return fio2; }
+            set { fio2 = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        /// <summary>
+        /// Конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="Fio2"></param>
+        /// <param name="Height"></param>
+        /// <param name="Weight"></param>
+        public Sport(string Fio2,int Height,double Weight)
+        {
+            this.Fio2 = fio2;
+            this.Height = height;
+            this.Weight = weight;
+        }
+        
+        /// <summary>
+        /// Метод вывода
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Ф.И.О - {fio2}\nРост = {height}\nВес = {weight}");
+        }
+        
+
+
+
+
+
+
+    }
+}
